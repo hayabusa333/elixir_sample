62833cb1263922884055d0eb9d98b5c71020fe97
hayabusa333 <junnichi0121@gmail.com>
2020-09-26 10:35:54 +0900

    init commit

---
+++ lib/elixir_sample.ex
@@ -0,0 +1,18 @@
+defmodule ElixirSample do
+  @moduledoc """
+  Documentation for ElixirSample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirSample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
