a437f9d8c4d7b5c2a8f70e2fd9099892032e6dc7
hayabusa333 <junnichi0121@gmail.com>
2020-09-28 19:47:59 +0900

    add node broadcast ping

--- lib/recv_node.ex
+++ lib/recv_node.ex
@@ -2,4 +2,8 @@ defmodule RecvNode do
   def start() do
     {:ok, _} = Node.start(:recv, :shortnames, 15000)
   end
+
+  def self() do
+    Node.self
+  end
 end
--- lib/send_node.ex
+++ lib/send_node.ex
@@ -2,4 +2,18 @@ defmodule SendNode do
   def start() do
     {:ok, _} = Node.start(:send, :shortnames, 15000)
   end
+
+  def self() do
+    Node.self()
+  end
+
+  def list do
+    Node.list()
+  end
+
+  def broadcast_ping do
+    for node_list <- list do
+      Node.ping(node_list)
+    end
+  end
 end
