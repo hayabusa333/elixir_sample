e08e59fe8c7807b32a4f0ca105c64e957fc4846f
hayabusa333 <junnichi0121@gmail.com>
2020-10-06 21:08:51 +0900

    add add methods

--- lib/recv_process.ex
+++ lib/recv_process.ex
@@ -13,4 +13,12 @@ defmodule RecvProcess do
       1_000 -> "nothing after 1s"
     end
   end
+
+  def add do
+    receive do
+      {val, num} -> {:ok, val + num}
+    after
+      1_000 -> {:error}
+    end
+  end
 end
