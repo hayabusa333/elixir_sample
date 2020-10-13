162f63eaaf20e5897c53f6607adb360c55d2a285
hayabusa333 <junnichi0121@gmail.com>
2020-10-14 08:31:00 +0900

    add guard

--- lib/sample_state.ex
+++ lib/sample_state.ex
@@ -2,7 +2,11 @@ defmodule SampleState do
   @enforce_keys [:val]
   defstruct [:val]
 
-  def new(val) do
+  def new(val) when is_integer(val) do
     {:ok, %SampleState{val: val}}
+  end
+
+  def new(_) do
+    {:error}
   end
 end
