3b048c15fad91c9ba4ed26702304b66c7d9a8a9f
hayabusa333 <junnichi0121@gmail.com>
2020-10-15 20:57:15 +0900

    add num

--- lib/sample_state.ex
+++ lib/sample_state.ex
@@ -1,6 +1,10 @@
 defmodule SampleState do
   @enforce_keys [:val]
-  defstruct [:val]
+  defstruct [:val, :num]
+
+  def new(val, num) when is_integer(val) do
+    {:ok, %SampleState{val: val, num: num}}
+  end
 
   def new(val) when is_integer(val) do
     {:ok, %SampleState{val: val}}
